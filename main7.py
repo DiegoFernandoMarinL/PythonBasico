df0c51352685a281103152575c4084f90a2ead33
DiegoFernandoMarinL <160855594+DiegoFernandoMarinL@users.noreply.github.com>
2024-02-23 19:42:50 -0500

    Ejercicio 7

---
+++ main7.py
@@ -0,0 +1,7 @@
+base = input("Digite base del triangulo: ")
+altura = input("Digite altura del triangulo: ")
+
+area = int(base)*int(altura)
+area = area/2
+
+print("El area del triengulo es: ",area)
