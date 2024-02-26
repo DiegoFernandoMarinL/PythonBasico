c73ca4133fe7f7327bbcad65bb7d0c5314edb164
DiegoFernandoMarinL <160855594+DiegoFernandoMarinL@users.noreply.github.com>
2024-02-26 13:26:39 -0500

    Ejercicio 8 Correcion

--- main8.py
+++ main8.py
@@ -1,14 +1,10 @@
-serie = []
-serieFibo = []
-for i in range(15):
-    serie.append(i)
+a = 0
+b = 1
+contador = 0
 
-for a in range(15):
-    suma = int(serie[a]) + int(serie[a-1])
-    serieFibo.append(suma)
+while contador < 20:
+    print(a)
+    a, b = b, b + a
+    contador = contador + 1
 
-   
-print("1 ,",serieFibo[0],",",serieFibo[1],",",serieFibo[2],",",serieFibo[3],",",serieFibo[4],",",serieFibo[5],",",
-      serieFibo[6],",",serieFibo[7],",",serieFibo[8],",",serieFibo[9],",",
-      serieFibo[10],",",serieFibo[11],",",serieFibo[12],",",serieFibo[13],",",serieFibo[14])   
   
